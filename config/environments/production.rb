bd179c4b0aff5e013262e85bef79d390f73797e1
Jennifer Konikowski <jmkoni@icloud.com>
2025-08-12 15:21:12 -0400

    remove render and add mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,11 @@ Rails.application.configure do
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = {host: "example.com"}
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: ENV["MAILGUN_API_KEY"],
+    #domain: "fingerlakesevents.com"
+  }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
